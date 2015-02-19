466992d2e76c76baf0efdfbb8bc83206416fa674
desiresdesigner <rumeantseva.e@gmail.com>
2015-02-19 13:11:14 +0300

    Getting percents of passed tests from Fitness answers in testCaller

--- src/main/java/osiristherNative/testerCaller.java
+++ src/main/java/osiristherNative/testerCaller.java
@@ -4,6 +4,9 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.text.DecimalFormat;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by desiresdesigner on 17.02.15.
@@ -12,29 +15,48 @@ public class testerCaller {
 
     public String testExec(String execName, int taskID){
         try {
-            //"java -DEXEC='ex/main.o' -jar  ~/soft/FitNesse/fitnesse-standalone.jar -d ~/Projects/Osiristher -c \"example?test&format=text\""
             System.out.println( "java -DEXEC='" + execName +
                     "' -jar  ~/soft/FitNesse/fitnesse-standalone.jar -d ~/Projects/Osiristher -c '" + taskID + "?test&format=text'");
             ProcessBuilder pb = new ProcessBuilder("/bin/bash", "-c", "java -DEXEC='" + execName +
                     "' -jar  ~/soft/FitNesse/fitnesse-standalone.jar -d ~/Projects/Osiristher -c '" + taskID + "?test&format=text'");
-            //pb.directory(new File("/home/desiresdesigner/Projects/Osiristher/src/main/java/osiristherNative/CompileScripts")); // ToDo: remember to change this dependency when deploy it on alert server
             Process p = pb.start();
 
             BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
-            BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+            //BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
 
             String s;
             while ((s = reader.readLine()) != null) {
-                System.out.println("Script output: " + s);
+                parseLine(s);
+                //System.out.println("Script output: " + s);
             }
-            String errors = "";
+            /*String errors = "";
             while ((s = stdError.readLine()) != null) {
                 System.out.println("Script error_output: " + s);
                 errors += s;
-            }
+            }*/
         } catch (IOException e) {
             e.printStackTrace();
         }
         return ""; // ToDo correct
+    }
+
+    private String parseLine(String line){
+        if (line.startsWith(".")){
+            System.out.println("Correct");
+        } else if (line.startsWith("F")){
+            Pattern pattern = Pattern.compile("R:([0-9]*)");
+            Matcher matcher = pattern.matcher(line);
+            matcher.find();
+            float right = Integer.parseInt(matcher.group(1));
+
+            pattern = Pattern.compile("W:([0-9]*)");
+            matcher = pattern.matcher(line);
+            matcher.find();
+            float wrong = Integer.parseInt(matcher.group(1));
+            return new DecimalFormat("##.##").format((right / (wrong + right) * 100)) + "% of tests passed";
+        } else if (line.startsWith("X")){
+            return "Exception in fixture";
+        }
+        return "FATAL ERROR";
     }
 }
